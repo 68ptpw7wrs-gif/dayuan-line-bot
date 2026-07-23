292fb1154d969685249b857fb271d4e4504e4238
68ptpw7wrs-gif <68ptpw7wrs@privaterelay.appleid.com>
2026-07-23 18:16:53 +0800

    Add calendar image replies

--- reply.js
+++ reply.js
@@ -2,12 +2,19 @@ const TRIGGERS = new Set(["你好", "哈囉", "hello", "hi", "ping"]);
 
 const REGISTRATION_WORDS = ["報名", "單兵", "候補", "收單兵"];
 const TIME_WORDS = ["何時", "什麼時候", "哪天", "幾點", "開放", "開始", "時間", "本週有開嗎", "這週有開嗎"];
+const CALENDAR_QUESTIONS = ["本週有嗎", "這週有嗎", "本週有球嗎", "這週有球嗎", "本週有開嗎", "這週有開嗎", "球局行事曆", "行事曆"];
+const CALENDAR_URL = "https://dayuan-legends.v2rc2d7trr.chatgpt.site/history";
+const CALENDAR_IMAGE_URL = "https://dayuan-legends.v2rc2d7trr.chatgpt.site/calendar-current.png";
 
 function asksRegistrationTime(text) {
   return (
     REGISTRATION_WORDS.some((word) => text.includes(word)) &&
     TIME_WORDS.some((word) => text.includes(word))
   );
+}
+
+function asksForCalendar(text) {
+  return CALENDAR_QUESTIONS.some((question) => text.includes(question));
 }
 
 function createReply(text) {
@@ -28,4 +35,18 @@ function createReply(text) {
   return "哈囉！我是剛做好的 LINE 機器人 🤖";
 }
 
-module.exports = { createReply };
+function createMessages(text) {
+  const normalized = text.trim().toLowerCase();
+
+  if (asksForCalendar(normalized)) {
+    return [
+      { type: "image", originalContentUrl: CALENDAR_IMAGE_URL, previewImageUrl: CALENDAR_IMAGE_URL },
+      { type: "text", text: `最新球局行事曆：\n${CALENDAR_URL}` },
+    ];
+  }
+
+  const replyText = createReply(text);
+  return replyText ? [{ type: "text", text: replyText }] : null;
+}
+
+module.exports = { createMessages, createReply };
